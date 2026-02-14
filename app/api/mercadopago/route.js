4a38d3bb0d96aebabc6db4090333781399bb5a0d
aliriodi <aliriodi@gmail.com>
2026-02-14 14:03:24 -0300

    primera prueba con BD v10

--- app/api/mercadopago/route.js
+++ app/api/mercadopago/route.js
@@ -20,18 +20,18 @@ export async function POST(req) {
     // Conectar DB
     await dbConnect();
  // 1) Guardar SIEMPRE el evento crudo
-//  const eventDoc = await MercadoPagoEvent.create({
-//     eventId: String(body?.id || ""),
-//     paymentId: paymentId || undefined,
-//     action: body?.action,
-//     type: body?.type || body?.topic,
-//     api_version: body?.api_version,
-//     live_mode: body?.live_mode,
-//     user_id: body?.user_id,
-//     date_created: body?.date_created ? new Date(body.date_created) : new Date(),
-//     payload: body,
-//     lookup_ok: false,
-//   });
+ const eventDoc = await MercadoPagoEvent.create({
+    eventId: String(body?.id || ""),
+    paymentId: paymentId || undefined,
+    action: body?.action,
+    type: body?.type || body?.topic,
+    api_version: body?.api_version,
+    live_mode: body?.live_mode,
+    user_id: body?.user_id,
+    date_created: body?.date_created ? new Date(body.date_created) : new Date(),
+    payload: body,
+    lookup_ok: false,
+  });
 //   //las lineas anteriores se van a eliminar
   // al ver que el evento se guarde 
   //y se ponga ne produccion
